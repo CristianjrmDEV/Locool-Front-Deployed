749a3839de2dd8650234c76cad31ac6d6cae4abc
VitoCorleone2022 <javier.civic@gmail.com>
2023-04-26 14:49:31 +0100

    Project setup: - Vite - React router dom - Axios - MUI - Fonts: Dosis, Quicksand - Folder structure - Prettier file & package.json config

---
+++ Locool/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit iusto
+        error tenetur voluptatibus nobis, recusandae minima fuga iure
+        dignissimos sapiente. Suscipit error voluptatem harum tempore cumque
+        atque! Placeat, suscipit inventore!
+      </div>
+    </>
+  )
+}
+
+export default App
