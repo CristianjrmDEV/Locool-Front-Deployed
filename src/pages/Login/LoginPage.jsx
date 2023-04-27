e504fbf2ef7d959771c498d0d516e17bb90eaca9
VitoCorleone2022 <javier.civic@gmail.com>
2023-04-27 16:19:16 +0100

    Changes: -  Service logout & sign up - Pages: Login, Basic, Farm, Signup, - Layout: Basic, Main - Components: Login, LogoutButton,

---
+++ src/pages/Login/LoginPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './LoginPage.css'
+import LoginComponent from '../../components/Login/LoginComponent'
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>LoginPage</h1>
+      <LoginComponent />
+    </div>
+  )
+}
+
+export default LoginPage
