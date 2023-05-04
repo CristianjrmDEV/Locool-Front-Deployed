7052fa7461f0b92c2982db4488a3bb060c6db434
CristianjrmDEV <cristianjrm.dev@gmail.com>
2023-05-04 16:38:40 +0100

    Added paypal payment with account and password, added routes for success, fail and payment method

--- src/components/PaymentSelection/PaymentSelectionComponent.jsx
+++ src/components/PaymentSelection/PaymentSelectionComponent.jsx
@@ -9,40 +9,31 @@ import { faCcPaypal } from '@fortawesome/free-brands-svg-icons';
 import { faCcMastercard } from '@fortawesome/free-brands-svg-icons';
 import { faCcVisa } from '@fortawesome/free-brands-svg-icons';
 import { PayPalScriptProvider, PayPalButtons } from '@paypal/react-paypal-js';
+import { useNavigate } from 'react-router-dom';
+
+
 
 const PaymentSelectionComponent = () => {
+  const navigate = useNavigate()
+
   return (
     <Card sx={{ border: 'none', boxShadow: 'none', maxWidth: 477, margin: '0 auto', padding: '1em' }} >
       <CardHeader title="Choose payment method" sx={{ color: 'red.main', fontSize: '48px' }} titleTypographyProps={{ variant: 'h5', fontFamily: 'dosis', marginBottom: '14px' }} />
       <CardContent>
+        {
+          /**Datos para probar el pago con paypal:
+           * correo: sb-gyd0c25884475@personal.example.com
+           * contraseña: %5)9Uqmp
+           */
+        }
+        <PayPalScriptProvider options={{'client-id': "AWBz22U-IiFbvZ-YGLGI1r1jsPcL7Oc7zZ-rNwl8vlyP7q-goquqs-GjmIUYRBHZ_ZZM80_c6f_301hZ"}}>
+          <PayPalButtons 
+            onApprove={(data, actions) => {
+              navigate('/success');
+            }}
+          />
+        </PayPalScriptProvider>
 
-          <Button sx={{backgroundColor: 'secondary.main', width: '100%', textTransform: 'capitalize', justifyContent: "flex-start", marginBottom: '20px'}}>
-            <Typography sx={{ fontSize: 18 }} color="text.secondary" gutterBottom>
-            <FontAwesomeIcon icon={faCcPaypal} color='#253b80'/>
-
-              Paypal
-            </Typography>
-
-          </Button>
-          <PayPalScriptProvider options={{'client-id': "sb-xryib25894968@business.example.com"}}>
-            <PayPalButtons/>
-          </PayPalScriptProvider>
-          <Button sx={{backgroundColor: 'secondary.main', width: '100%', textTransform: 'capitalize', justifyContent: "flex-start"}}>
-            <Typography sx={{color: 'primary.main', fontSize: 18, textAlign:'left'}} color="text.secondary" gutterBottom>
-              <FontAwesomeIcon icon={faCcVisa} color='#192061' />
-              <FontAwesomeIcon icon={faCcMastercard} color="#FF5F00"/>
-              Debit or credit card
-            </Typography>
-          </Button>
-          <Button
-          color="white"
-          sx={{ height: '39.41px', margin:'0 auto', maxWidth: '100%', margin: '26px 0 0 0', borderRadius:'6px', backgroundColor: 'green.main', width: '100%', fontSize: '14px', textTransform: 'capitalize', fontWeight: 'bold',':hover': {
-            bgcolor: 'primary.main', // theme.palette.primary.main
-            color: 'white',
-          }, }}
-        >
-          Pay
-        </Button>
       </CardContent>
     </Card>
   );
