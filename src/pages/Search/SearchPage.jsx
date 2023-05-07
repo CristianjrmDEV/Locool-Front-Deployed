28d84c3c077ce442a3ce0b743f3335f2fa8d7ffc
VitoCorleone2022 <javier.civic@gmail.com>
2023-05-07 22:17:26 +0100

    ProductList

--- src/pages/Search/SearchPage.jsx
+++ src/pages/Search/SearchPage.jsx
@@ -4,10 +4,9 @@ import { useContext } from 'react'
 import { Box, Typography } from '@mui/material'
 import { FarmsContext } from '../../contexts/farm'
 import { ProductsContext } from '../../contexts/product'
-import FarmCardComponent from '../../components/FarmCard/FarmCardComponent'
-import ProductCardComponent from '../../components/ProductCard/ProductCardComponent'
 import PageTitleComponent from '../../components/PageTitle/PageTitleComponent'
 import FarmListComponent from '../../components/FarmList/FarmListComponent'
+import ProductListComponent from '../../components/ProductList/ProductListComponent'
 
 const SearchPage = () => {
   const GLOBAL_Product = useContext(ProductsContext)
@@ -15,19 +14,7 @@ const SearchPage = () => {
 
   const displayProducts = () => {
     if (GLOBAL_Product.get.length > 0) {
-      return (
-        <Box>
-          <Typography variant="h6">Products</Typography>
-          {GLOBAL_Product.get.map((product, idx) => {
-            return (
-              <ProductCardComponent
-                key={idx}
-                product={product}
-              />
-            )
-          })}
-        </Box>
-      )
+      return <ProductListComponent products={GLOBAL_Product} />
     }
   }
 
