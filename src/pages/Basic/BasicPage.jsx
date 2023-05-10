ebb718054cd61a61ce10a38c79f3c873902cba6c
Cristian Rivero <cristianjrm.dev@gmail.com>
2023-05-10 16:39:04 +0100

    Adding carouseñ

--- src/pages/Basic/BasicPage.jsx
+++ src/pages/Basic/BasicPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './BasicPage.css'
 import PageTitleComponent from '../../components/PageTitle/PageTitleComponent'
-
+import DemoCarousel from '../../components/DemoCarousel/DemoCarouselComponent'
 const BasicPage = () => {
   return (
     <>
       <PageTitleComponent title={'Basic page'} />
+      <DemoCarousel/>
     </>
   )
 }
